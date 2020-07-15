29cc83988e9dac5070b2b90e448ae4012af682eb
vhamilton08 <vhamilton08@gmail.com>
2020-07-15 15:17:27 -0500

    checking

---
+++ Part2/index.js
@@ -0,0 +1,7 @@
+const idInput = document.getElementById("idInput")
+const colorInput = document.getElementById("colorInput")
+
+function setCard() {
+    const card = document.getElementById("idInput.value")
+    card.style.color = colorInput.value
+}
